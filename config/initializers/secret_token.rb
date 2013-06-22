dcc9ebd4b199e30336711d37c19f681d324fb08b
jmiramant <josh@miramant.me>
2013-06-22 13:19:23 -0700

    NetTuts Sample App

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tasks::Application.config.secret_token = 'c74658ee61d175fc5986f7c595ea83a67e483e7ef28504f06a03179c6f3e4999d5b0726d384ff6f21e1ab4f52243328f4a25e969c2e26d62a9239d3e16c91f4c'
